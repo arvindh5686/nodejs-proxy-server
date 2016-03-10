72fe68a0d692c1f357bfd04947ae77ae55fb30a3
Aravindh Balakrishnan <abalakrishnan0@walmartlabs.com>
2016-03-10 06:41:35 -0800

    add logging and logic to send host as part of request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let http = require('http'),
 	request = require('request'),
 	argv = require('yargs').argv;
 
-let host = argv.host ? argv.host : '127.0.0.1',
+let host = argv.host || '127.0.0.1',
 	protocol = 'http://',
 	port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80),
 	destinationUrl = protocol + host + ':' + port;
@@ -17,12 +17,19 @@ let server = http.createServer((req, res) => {
 }).listen(8000);
 
 http.createServer((req, res) => {
-  let options = {
-  	headers: req.headers,
-    url: `${destinationUrl}${req.url}`
-  }
+	process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
+	req.pipe(process.stdout)
 
-  options.method = req.method;
+	destinationUrl = req.headers['x-destination-url'] || destinationUrl
 
-  req.pipe(request(options)).pipe(res);
+  	let options = {
+  		headers: req.headers,
+    	url: `${destinationUrl}${req.url}`,
+    	method: req.method
+  	}
+
+  	let downstreamResponse = req.pipe(request(options))
+	process.stdout.write(JSON.stringify(downstreamResponse.headers))
+	downstreamResponse.pipe(process.stdout)
+	downstreamResponse.pipe(res)
 }).listen(8001)
