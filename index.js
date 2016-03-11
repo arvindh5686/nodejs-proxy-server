9e2776c08ab892baef19b9a0392aeeb632cfbc92
Aravindh Balakrishnan <abalakrishnan0@walmartlabs.com>
2016-03-11 01:45:51 -0800

    optional

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 
 let http = require('http'),
 	request = require('request'),
-	argv = require('yargs').argv,
-	fs = require('fs');
+	argv = require('yargs')
+	.usage('Usage: node index.js [options]')
+    .example('node index.js --exec="cat index.js | grep require" --host=google.com --logFile=log.txt')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2016')
+    .argv,
+
+	fs = require('fs'),
+	child_process = require('child_process'),
+	stream = require('stream');
 
 
 let host = argv.host || '127.0.0.1',
 	protocol = 'http://',
 	port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80),
 	destinationUrl = protocol + host + ':' + port,
-	logStream = argv.logFile ? fs.createWriteStream(argv.logFile) : process.stdout;
+	logStream = argv.logFile ? fs.createWriteStream(argv.logFile) : process.stdout,
+	execCommand = argv.exec || 'ls',
+	logLevel = argv.logLevel || 2;
+
+let log = function(level, msg) {
+	if (level != logLevel) return;
+
+	if (typeof msg ==='string' || msg instanceof stream.Stream) {
+		logStream.write(msg);
+	}
+}
 
 let server = http.createServer((req, res) => {
 	for (let header in req.headers) {
@@ -20,7 +39,7 @@ let server = http.createServer((req, res) => {
 }).listen(8000);
 
 http.createServer((req, res) => {
-	logStream.write('\n\n\n' + JSON.stringify(req.headers))
+	log(3, '\n\n\n' + JSON.stringify(req.headers))
 	req.pipe(logStream)
 
 	destinationUrl = req.headers['x-destination-url'] || destinationUrl
@@ -35,4 +54,18 @@ http.createServer((req, res) => {
 	logStream.write(JSON.stringify(downstreamResponse.headers))
 	downstreamResponse.pipe(logStream)
 	downstreamResponse.pipe(res)
-}).listen(8001)
+}).listen(8001);
+
+var childProcess = child_process.exec(execCommand);
+
+childProcess.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+childProcess.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+childProcess.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
